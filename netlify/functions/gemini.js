777cc03de71280008911f536df294cb759a44fbb
Backerjr <ahmedbackerjr@gmail.com>
2025-09-27 11:34:12 +0400

    Update gemini.js
    Signed-off-by: Backerjr <ahmedbackerjr@gmail.com>

--- netlify/functions/gemini.js
+++ netlify/functions/gemini.js
@@ -1,5 +1,3 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
 module.exports.handler = async function (event) {
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
